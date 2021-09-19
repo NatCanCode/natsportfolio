42e652b1521e571e8de14856769b21b9345df451
NatCanCode <77299658+NatCanCode@users.noreply.github.com>
2021-09-19 18:14:11 +0200

    Update french.js

--- script/french.js
+++ script/french.js
@@ -316,5 +316,30 @@ anime.timeline({loop: true})
     })
 
 
-// console.log(++[[]][+[]]+[+[]]);
-// outputs 10
+// Cursor coloured animation
+const cursor = document.querySelector('.cursor')
+let timeout 
+
+// follow cursor on mousemove
+document.addEventListener('mousemove', (e) => {
+    // let x = e.pageX
+    // let y = e.pageY
+    // if issues when hovering over links or scrolling, replace with:
+    let x = e.clientX
+    let y = e.clientY
+    cursor.style.top = y + 'px'
+    cursor.style.left = x + 'px'
+    cursor.style.display = 'block'
+
+    // cursor effects on mouse stopped
+    function mouseStopped(){
+        cursor.style.display = 'none'
+    }
+    clearTimeout(timeout)
+    timeout = setTimeout(mouseStopped, 1000)
+})
+
+// cursor effects on mouseout
+document.addEventListener('mouseout', () => {
+    cursor.style.display = 'none'  
+})
