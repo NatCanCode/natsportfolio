e4692a252b62c656fa80310e7d04f45af9a4b329
NatCanCode <77299658+NatCanCode@users.noreply.github.com>
2021-09-26 17:34:43 +0200

    Add files via upload

--- script/app.js
+++ script/app.js
@@ -58,23 +58,43 @@ greetings()
 
 // letter animation
 // wrap every letter in a span
-const textWrapper = document.querySelector('.work .letters')
+// const textWrapper = document.querySelector('.work .letters')
+// textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>")
+// anime.timeline({loop: true})
+//     .add({
+//         targets: '.work .letter',
+//         rotateY: [-90, 0],
+//         duration: 1300,
+//         delay: (el, i) => 45 * i
+//     })
+//     .add({
+//         targets: '.work',
+//         opacity: 0,
+//         duration: 1000,
+//         easing: "easeOutExpo",
+//         delay: 1000
+//     })
+
+const textWrapper = document.querySelector('.work')
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>")
 anime.timeline({loop: true})
-    .add({
-        targets: '.work .letter',
-        rotateY: [-90, 0],
-        duration: 1300,
-        delay: (el, i) => 45 * i
-    })
-    .add({
-        targets: '.work',
-        opacity: 0,
-        duration: 1000,
-        easing: "easeOutExpo",
-        delay: 1000
-    })
-
+  .add({
+    targets: '.work .letter',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: (el, i) => 500 + 30 * i
+  })
+  .add({
+    targets: '.work .letter',
+    translateX: [0,-30],
+    opacity: [1,0],
+    easing: "easeInExpo",
+    duration: 1100,
+    delay: (el, i) => 100 + 30 * i
+  })
 
 // carousel
 let carousel = document.getElementById("carousel")
@@ -316,23 +336,45 @@ let countdowns = document.querySelectorAll('[data-countdown]')
 }
 
 
+// const textWrap = document.querySelector('.touch')
+// textWrap.innerHTML = textWrap.textContent.replace(/\S/g, "<span class='letter'>$&</span>")
+// anime.timeline({loop: true})
+//     .add({
+//         targets: '.touch .letter',
+//         translateY: [-60,0],
+//         easing: "easeOutExpo",
+//         duration: 1400,
+//         delay: (el, i) => 30 * i
+//     })
+//     .add({
+//         targets: '.touch',
+//         opacity: 0,
+//         duration: 1000,
+//         easing: "easeOutExpo",
+//         delay: 1000
+//     })
+
+// Wrap every letter in a span for the "Get in touch" letter animation with anime
 const textWrap = document.querySelector('.touch')
 textWrap.innerHTML = textWrap.textContent.replace(/\S/g, "<span class='letter'>$&</span>")
 anime.timeline({loop: true})
-    .add({
-        targets: '.touch .letter',
-        translateY: [-60,0],
-        easing: "easeOutExpo",
-        duration: 1400,
-        delay: (el, i) => 30 * i
-    })
-    .add({
-        targets: '.touch',
-        opacity: 0,
-        duration: 1000,
-        easing: "easeOutExpo",
-        delay: 1000
-    })
+  .add({
+    targets: '.touch .letter',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: (el, i) => 500 + 30 * i
+  })
+  .add({
+    targets: '.touch .letter',
+    translateX: [0,-30],
+    opacity: [1,0],
+    easing: "easeInExpo",
+    duration: 1100,
+    delay: (el, i) => 100 + 30 * i
+  })
 
 
 // Cursor coloured animation
